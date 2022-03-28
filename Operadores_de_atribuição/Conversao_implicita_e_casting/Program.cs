4235da8aa2bac35a1b128147f139c973b77393b2
Danielle Barros Bassetto <danibbassetto@hotmail.com>
2022-03-28 16:45:53 -0300

    atualizando

---
+++ Operadores_de_atribuição/Conversao_implicita_e_casting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Conversao_implicita_e_casting {
+    class Program {
+        static void Main(string[] args) {
+
+            float x = 4.5f;
+            double y = x;  // Possivel pois o float tem 4 bits e o double tem 8
+            Console.WriteLine(y);
+          
+            double a;
+            float b;
+            a = 5.1;
+            b = (float)a;  // Aparece erro, pois double não 'cabe' em float! Assim, eu posso converter apenas por casting (uso o float na frente)
+            Console.WriteLine(b);
+
+           
+            double c;
+            int d;
+            c = 5.1;
+            d = (int)c;  // O mesmo ocorre com o int, pois o double tem valor quebrado, enquanto o int não tem. Os valores decimais serão excluidos.
+            Console.WriteLine(d);
+
+            int e = 5;
+            int f = 2;
+            double resultado = e / f; // Como e e f são int, o compilador entende que vc quer fazer uma divisão inteira, por isso só aparece a parte inteira.
+            Console.WriteLine(resultado);
+            double resultado2 = (double)e / f;  // Agora foi convertido e terá decimais
+            Console.WriteLine(resultado2);
+        }
+    }
+}
---
+++ Operadores_de_atribuição/Operadores_aritmeticos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Operadores_aritmeticos {
+    class Program {
+        static void Main(string[] args) {
+            /*
+                    OPERADOR        SIGNIFICADO
+                        +             adição
+                        -           subtração
+                        *           multiplicação
+                        /            divisão
+                        %         resto da divisão
+
+            * % / tem precedência maior que + -
+            */
+
+            int n1 = 3 + 4 * 2;
+            Console.WriteLine(n1);
+
+            int n2 = (3 + 4) * 2;
+            Console.WriteLine(n2);
+
+            int n3 = 17 % 3;
+            Console.WriteLine(n3);
+
+            int n4 = 10 / 8;
+            Console.WriteLine(n4);
+
+            float n5 = 10 / 8f;
+            Console.WriteLine(n5);
+
+            double n6 = (double) 10 / 8;
+            Console.WriteLine(n6);
+            
+            // OU
+            
+            double n7 = 10.0 / 8;  // Nesse exemplo, pelo menos um dos dois deve ter o '.0' para indicar que é double
+            Console.WriteLine(n7);
+
+            // exemplo de baskara
+
+            double a = 1.0, b = -3.0, c = -4.0;
+            double delta = Math.Pow(b, 2.0) - 4 * a * c;
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+            Console.WriteLine(delta);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+        }
+    }
+}
